4a6ca184b551c0488d28c08d15942878f7f6baff
Robertson <Robertson.Melissa1@mayo.edu>
2022-01-16 20:30:01 -0600

    deliverable2

--- Challenge/MechaCarChallenge.R
+++ Challenge/MechaCarChallenge.R
@@ -4,3 +4,19 @@ library(tidyverse)
 mecha_car_mpg<-read.csv('MechaCar_mpg.csv',check.names = F,stringsAsFactors = F)#import files
 model<-lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,data=mecha_car_mpg)#generate summary statistics
 summary(model)#summary stats
+model
+
+
+#######################################################
+#suspension coil dataset analysis
+######################################################
+
+coil_data<-read.csv('Suspension_Coil.csv',check.names=F, stringsAsFactors = F)
+
+total_summary<-coil_data%>%summarize(mean(PSI),median(PSI),var(PSI),sd(PSI),n())
+
+lot_summary<- coil_data%>%group_by(Manufacturing_Lot) %>% summarise(mean(PSI),median(PSI),var(PSI),sd(PSI),n())
+
+total_summary
+
+lot_summary
